b205b61dc4c862f9b37851b10ae4d30b8212548c
yannickwnz <yannickwnz@gmail.com>
2023-02-03 20:31:12 +0000

    php file

--- include/apiRequest.inc.php
+++ include/apiRequest.inc.php
@@ -5,10 +5,13 @@ if(isset($_GET['url'])) {
     $link = $_GET['url'];
 }
 
+if(!filter_var($link, FILTER_VALIDATE_URL)) {
+    echo "err";
+    return false;
+}
+
 $ch = curl_init(); 
 
-// $url = 'https://api.shrtco.de/v2/shorten?url=plasticandsave.com';
-// $url = 'https://api.shrtco.de/v2/shorten?url=invalid';
 $url = 'https://api.shrtco.de/v2/shorten?url=' . $link;
 
 curl_setopt($ch, CURLOPT_URL, $url);
@@ -18,8 +21,4 @@ $data = curl_exec($ch);
 
 curl_close($ch);
 
-// echo json_encode($data);
-
 echo $data;
-
-// var_dump($data);
